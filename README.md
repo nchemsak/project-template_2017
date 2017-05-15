0a18a7e568ff4b0e773a27cd6a36c5bd2dccae26
Nick Chemsak <nchemsak@gmail.com>
2017-05-14 19:45:47 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Title
+* Description
+
+## Installation
+* $ npm install
+
+## Usage
+* grunt
+
---
+++ javascripts/main.js
@@ -0,0 +1,7 @@
+"use strict";
+
+function add(a, b) {
+    return a + b;
+}
+console.log("add(5, 12): ", add(5, 12));
+
